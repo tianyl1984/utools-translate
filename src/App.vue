3cc4b9edb1ae6ef1b2decce694dd422d8cf30d25
tyl <tianyl1984@qq.com>
2025-02-27 16:54:58 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import Translate from './translate/index.vue'
+
+const route = ref('')
+const enterAction = ref({})
+
+onMounted(() => {
+  window.utools.onPluginEnter((action) => {
+    route.value = action.code
+    enterAction.value = action
+  })
+  window.utools.onPluginOut((isKill) => {
+    route.value = ''
+  })
+})
+</script>
+
+<template>
+  <template v-if="route === 'translate'">
+    <Translate :enterAction="enterAction"></Translate>
+  </template>
+</template>
